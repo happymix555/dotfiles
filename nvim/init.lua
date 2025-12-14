61591d3709c04d3abeaafa1305820d7db85e88e5
Chaiyaphat Pakthongchai <happymix2555@gmail.com>
2025-12-14 19:23:51 +0700

    user Lazy.nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,25 +1,2 @@
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
--- show line number on the left hand side
-vim.opt.number = true
--- show invisible characters like tab, space, etc.
-vim.o.list = true
--- defind how invisible characters are displayed
-vim.opt.listchars = {
-	tab = "> ", -- ✅ must be exactly two characters
-	trail = "·",
-	extends = "›",
-	precedes = "‹",
-	nbsp = "␣",
-	space = ".",
-}
--- use system clipboard as a default
-vim.opt.clipboard = "unnamedplus"
--- load the lazy plugin manager
--- this will help load other plugins we installed via Lazy
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
