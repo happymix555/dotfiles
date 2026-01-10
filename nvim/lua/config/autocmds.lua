90bc4d2e2a24bb47d9c5f9923d43354585c732e7
happyMix <happymix2555@gmail.com>
2026-01-10 12:36:15 +0700

    change tab from 2 to 4 spaces and remove the duplicated tab setting

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -12,9 +12,9 @@ vim.api.nvim_create_autocmd({ "FileType", "BufEnter", "BufWinEnter" }, {
   pattern = "*",
   callback = function()
     vim.bo.expandtab = false
-    vim.bo.tabstop = 2
-    vim.bo.shiftwidth = 2
-    vim.bo.softtabstop = 2
+    vim.bo.tabstop = 4
+    vim.bo.shiftwidth = 4
+    vim.bo.softtabstop = 4
   end,
 })
 
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,11 +5,6 @@
 -- Disable auto-formatting on save
 vim.g.autoformat = false
 
--- Use TAB characters instead of spaces
-vim.opt.expandtab = false -- Insert actual tab characters (not spaces)
-vim.opt.tabstop = 2 -- A tab character displays as 2 spaces wide
-vim.opt.shiftwidth = 2 -- Indent with 2 spaces worth when using >> or <<
-vim.opt.softtabstop = 2 -- Pressing Tab inserts a tab character
 vim.opt.autoindent = true -- Copy indent from current line when starting a new line
 vim.opt.smartindent = true -- Smart autoindenting when starting a new line
 
